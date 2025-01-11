d2b68b71f86da9823f19ab398700d4cf8408b266
Akshai T R <99003728+akshaitr@users.noreply.github.com>
2025-01-11 22:58:31 +0530

    Update curry.js

--- src/curry.js
+++ src/curry.js
@@ -1,8 +1,10 @@
 function curry(fn) {
     return function curried(...args) {
         if(args.length >= fn.length) {
+            // If enough arguments have been passed, call the function
             return fn(...args);
         } else {
+            // Otherwise, return a function that collects more arguments
             return function (...newArgs) {
                 return curried(...args, ...newArgs);
             }
