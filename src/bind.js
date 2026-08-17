7cc7a9f8e808638302c2a5b31d1eeebb5de8bd6b
Akshai T R <99003728+akshaitr@users.noreply.github.com>
2026-08-18 00:15:25 +0530

    Enhance bind function with constructor support
    Refactor bind function to handle constructor calls and use apply.

--- src/bind.js
+++ src/bind.js
@@ -1,11 +1,19 @@
-function bind(context = {}, ...args) {
-  if (typeof this !== "function") {
-    throw new TypeError(this + "cannot be bound");
-  }
-  context.func = this;
-  return function (...newArgs) {
-    return context.func(...args, ...newArgs);
-  };
-}
+function bind(context, ...boundArgs) {
+    // 1. Ensure 'this' is a callable function
+    if(typeof this !== "function") {
+        throw new TypeError(`${this} is not callable`)
+    }
 
-export default bind;
+    const targetFunction = this;
+
+    // 2. return a new wrapper function
+    return function BoundFunction(...callArgs) {
+        // Handle when called as a constructor using `new`
+        if(new.target) {
+            return new targetFunction(...boundArgs, ...callArgs);
+        }
+
+        // Standard function call using .apply()
+        return targetFunction.apply(context, [...boundArgs, ...callArgs]);
+    }
+}
